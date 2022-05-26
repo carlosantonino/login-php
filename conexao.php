bfbbc169340edf48c8642a61ce8131dc1ee2c2dd
Carlos <72629502+carlosantonino@users.noreply.github.com>
2022-05-26 19:07:40 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,13 @@
+<?php 
+    $host = "localhost";
+    $user = "";
+    $pass = "";
+    $dbname = "";
+
+    try {
+        // Conexão 
+        $conn = new PDO("mysql:host=$host;dbname=" . $dbname, $user, $pass);
+        // echo "Conexão realizada com sucesso";
+    } catch (PDOException $err) {
+        // echo "Erro: Não conseguiu realizar a conexão";
+    }
